113cc2a3faf288352ce4be3b19184d079b480bad
Bruno <neckel.bw@gmail.com>
2024-03-18 17:55:34 -0300

    first commit

---
+++ src/domain/forum/application/use-cases/answer-question.ts
@@ -0,0 +1,33 @@
+import { UniqueEntityId } from '@/core/entities/unique-entity-id'
+import { AnswersRespository } from '../repositories/answers-repository'
+import { Answer } from '../../enterprise/entities/answer'
+
+interface AnswerQuestionUseCaseRequest {
+  instructorId: string
+  questionId: string
+  content: string
+}
+
+interface AnswerQuestionUseCaseResponse {
+  answer: Answer
+}
+
+export class AnswerQuestionUseCase {
+  constructor(private answersRepository: AnswersRespository) {}
+
+  async execute({
+    instructorId,
+    questionId,
+    content,
+  }: AnswerQuestionUseCaseRequest): Promise<AnswerQuestionUseCaseResponse> {
+    const answer = Answer.create({
+      content,
+      authorId: new UniqueEntityId(instructorId),
+      questionId: new UniqueEntityId(questionId),
+    })
+
+    await this.answersRepository.create(answer)
+
+    return { answer }
+  }
+}
---
+++ test/repositories/in-memory-questions-repository.ts
@@ -0,0 +1,20 @@
+import { QuestionsRespository } from '@/domain/forum/application/repositories/questions-repository'
+import { Question } from '@/domain/forum/enterprise/entities/question'
+
+export class InMemoryQuestionsRepository implements QuestionsRespository {
+  public items: Question[] = []
+
+  async findBySlug(slug: string) {
+    const question = this.items.find((item) => item.slug.value === slug)
+
+    if (!question) {
+      return null
+    }
+
+    return question
+  }
+
+  async create(question: Question) {
+    this.items.push(question)
+  }
+}
